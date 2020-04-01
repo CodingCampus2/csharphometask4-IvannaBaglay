282e077cc6cfbfadf72bd6aed7eedbe865c4ff66
IvannaB <ivankamorkai.baglay@gmail.com>
2020-04-01 21:26:05 +0300

    Added Game of Live

--- Program.cs
+++ Program.cs
@@ -5,6 +5,21 @@ namespace HomeworkTemplate
 {
     class Program
     {
+        const char LIVE = '+';
+        const char DEAD = '-';
+        static void GenerateRandomCharArray(char[,] array, int row, int column)
+        {
+            string chars = "0101010101010101";
+            Random rand = new Random();
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    int num = rand.Next(chars.Length);
+                    array[i, j] = chars[num];
+                }
+            }
+        }
         static int CalculateLivesNeighborhood(char[,] board, int x,  int y)
         {
             int row = board.GetLength(0);
@@ -38,11 +53,55 @@ namespace HomeworkTemplate
             }
             return current;
         }
+
+        static void CopyArray(char[,] arrayFrom, char[,] arrayTo, int row, int column)
+        {
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    arrayTo[i, j] = arrayFrom[i, j];
+                }
+            }
+        }
+        static void Show(char[,] array, int row, int column)
+        {
+            for (int i = row - 1; i >= 0; i--)
+            {
+                for (int j = column - 1; j >= 0; j--)
+                {
+                    Show(array[i, j], i, j);
+                }
+            }
+        }
+        static void GameOfLife()
+        {
+            int row = 100;
+            int column = 50;
+            char[,] newGeneration = new char[row, column];
+            GenerateRandomCharArray(newGeneration, row, column);
+
+            while (true)
+            {
+                char[,] currentGeneration = new char[row, column];
+                CopyArray(newGeneration, currentGeneration, row, column);
+                for (int i = 0; i < row; i++)
+                {
+                    for (int j = 0; j < column; j++)
+                    {
+                        newGeneration[i, j] = GetNextGenerate(currentGeneration, i, j);
+                    }
+                }
+                Show(newGeneration, row, column);
+            }
+        }
+
         static void Show (char cell, int x, int y )
         {
+            char symbol = (cell == '1') ? LIVE : DEAD;
             Console.SetCursorPosition(x, y);
             Console.CursorVisible = false;
-            Console.Write(cell);
+            Console.Write(symbol);
         }
         static void Main(string[] args)
         {
@@ -51,14 +110,9 @@ namespace HomeworkTemplate
                 int row = task.Board.GetLength(0);
                 int column = task.Board.GetLength(1);
                 char[,] board = new char[row, column];
+                char[,] old = new char[row, column];
 
-                for (int i = 0; i < row; i++)
-                {
-                    for (int j = 0; j < column; j++)
-                    {
-                        board[i, j] = task.Board[i,j];
-                    }
-                }
+                CopyArray(task.Board, board, row, column);
 
                 for (int i = 0; i < row; i++)
                 {
@@ -68,15 +122,7 @@ namespace HomeworkTemplate
                     }
                 }
 
-
-                for (int i = row-1; i >= 0; i--)
-                {
-                    for (int j = column -1; j >= 0; j--)
-                    {
-                        Show(board[i, j], i, j);
-                    }
-                }
-
+                // GameOfLife(); 
                 return board;
             };
             Task4.CheckSolver(TaskSolver);
